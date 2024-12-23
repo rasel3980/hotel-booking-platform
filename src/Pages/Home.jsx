5bb607ff4b69835eda2931a056cc3880ec8ff4d9
rasel3980 <rkrasel3980@gmail.com>
2024-12-23 16:51:47 +0600

    Firebase dot env setup

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Home = () => {
+    console.log(import.meta.env.VITE_apiKey);
     return (
         <div className='w-11/12 mx-auto'>
+            
         </div>
     );
 };
