73e674737db4a265e2794e4ad2d3b45a9ef5344a
rasel3980 <rkrasel3980@gmail.com>
2024-12-27 08:58:32 +0600

    Added a slider

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
 import { authContext } from '../AuthProvider/AuthProvider';
+import Slide from './Slide';
 
 const Home = () => {
     const {user}=useContext(authContext);
     return (
+        <>
+        <Slide></Slide>
         <div className='w-11/12 mx-auto'>
             name: {user?.displayName}
         </div>
+        </>
     );
 };
 
